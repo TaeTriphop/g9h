f40b122ca8f2431ecdd63c5c5c9a3db2c6702ee0
GPFBKK\triphop.pro <triphop.pro@gmail.com>
2023-09-19 17:57:22 +0700

    Update Portfolio

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,19 +1,16 @@
-import { projects } from "../../constants/portfolio"
-import ProjectContainer from '../ProjectContainer/ProjectContainer';
-import '../../Styles/Projects.css'
+import { projects } from "../../constants/portfolio";
+import ProjectContainer from "../ProjectContainer/ProjectContainer";
+import "../../Styles/Projects.css";
 
 export default function ProjectsPage() {
-  if (!projects.length) return null
+  if (!projects.length) return null;
   return (
-    <section id='projects' className='section projects'>
-      <h2 className='section__title'>Projects</h2>
-
-      <div className='projects__grid'>
-
+    <section id="projects" className="section projects">
+      <h2 className="section__title">Projects</h2>
+      <div className="projects__grid">
         {projects.map((project) => (
           <ProjectContainer key={project.projectId} project={project} />
         ))}
-        
       </div>
     </section>
   );
--- src/constants/portfolio.tsx
+++ src/constants/portfolio.tsx
@@ -69,7 +69,6 @@ const projects: interfaceProjects[] = [
   // },
 ]
 
-
 const skills: string[] = [
   'HTML',
   'CSS',
